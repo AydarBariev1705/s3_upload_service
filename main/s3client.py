c92a327c91ea3db393a8ce7ca75724b0f4e3e682
Aydar <168444560+AydarBariev1705@users.noreply.github.com>
2024-08-08 10:11:42 +0300

    Update s3client.py

--- main/s3client.py
+++ main/s3client.py
@@ -39,14 +39,4 @@ class S3Client:
         except ClientError as e:
             print(f"Error uploading file: {e}")
 
-# def main():
-#     s3_client = S3Client(
-#         access_key="7d12f2af5296405f9417d36169d11bc1",
-#         secret_key="18afe22c3f2049229a45dc49175983a1",
-#         endpoint_url="https://s3.storage.selcloud.ru",
-#         bucket_name="barievbucket1",
-#     )
-#     s3_client.upload_file("3333.jpg")
-#
-#
-# main()
+
